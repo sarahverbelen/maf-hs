690e2e1b11eb7b95c65b41bcfce0ade328ef3dd2
Sarah Verbelen <sarah.verbelen2000@gmail.com>
2024-03-08 16:40:34 +0100

    refinable lattice

--- maf2-slicer/src/Dependency/Atoms.hs
+++ maf2-slicer/src/Dependency/Atoms.hs
@@ -1,13 +1,22 @@
 module Dependency.Atoms where 
 
+import Domain
 import Lattice
 
--- an atom is an element a of a uco p such that there does not exist another element b with bot < b < a
 class (JoinLattice v) => AtomicLattice v where 
+-- | an atom is an element a such that there does not exist another element b with bot < b < a
     atom :: v -> Bool
-
 
 instance (Ord a, Show a) => AtomicLattice (CP a) where 
     atom Bottom = False 
     atom Top = False 
     atom (Constant _) = True
+
+class (JoinLattice v) => RefinableLattice v where 
+-- | refine returns a list of all elements that are smaller    
+    refine :: v -> [v] 
+
+instance (Ord a, Show a, Enum a, Bounded a) => RefinableLattice (CP a) where 
+    refine Bottom = []
+    refine (Constant _) = [Bottom]
+    refine Top = [Constant i | i <- [minBound..]]
