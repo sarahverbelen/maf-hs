f4c65f4d03b135910466cf045d38a64d0e70f3dc
Sarah Verbelen <sarah.verbelen2000@gmail.com>
2024-03-08 14:54:46 +0100

    atomic lattices

--- maf2-slicer/src/Dependency/Atoms.hs
+++ maf2-slicer/src/Dependency/Atoms.hs
@@ -3,6 +3,11 @@ module Dependency.Atoms where
 import Lattice
 
 -- an atom is an element a of a uco p such that there does not exist another element b with bot < b < a
-class (JoinLattice v) => Atomic v where 
+class (JoinLattice v) => AtomicLattice v where 
     atom :: v -> Bool
-  
+
+
+instance (Ord a, Show a) => AtomicLattice (CP a) where 
+    atom Bottom = False 
+    atom Top = False 
+    atom (Constant _) = True
