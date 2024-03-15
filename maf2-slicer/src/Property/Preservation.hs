a4fb9a55cc37eaf10227c159897026994ad60e83
Sarah Verbelen <sarah.verbelen2000@gmail.com>
2024-03-15 15:11:32 +0100

    basic property preservation

--- maf2-slicer/src/Property/Preservation.hs
+++ maf2-slicer/src/Property/Preservation.hs
@@ -9,34 +9,34 @@ import Control.Monad.State
 
 type PPState v = (AbstractSto Ide v, Agreement)
 
--- | PP(g, e)  TODO: add the predicate B
+-- | PP(g, e)  TODO: add the predicate B (?)
 preserve' :: (Eq v) => Exp -> State (PPState v) Bool
--- PP-ASSIGN
+-- | PP-ASSIGN
 preserve' (Dfv var e _) = do b <- preserve' e 
                              let v = undefined -- the value of the expression in the current state
                              (s, g) <- get
                              let b' = v `elem` Map.lookup var s -- check if the property is the same as before the assignment (if the var existed then)
                              put (Map.insert var v s, g) -- update the state to include this new binding for var
                              return $ b && b'
--- preserve' (Dff var ps e _) = TODO
+-- preserve' (Dff var args bdy _) = TODO
 preserve' (Set var e x) = preserve' (Dfv var e x)
--- -- | PP-LET *
--- preserve' (Let binds e _) = undefined
--- preserve' (Ltt binds e _) = undefined
--- preserve' (Ltr binds e _) = undefined
--- preserve' (Lrr binds e _) = undefined
+-- | PP-LET * (could modify the state in the body of the let)
+preserve' (Let binds bdy _) = preserve' bdy -- TODO: add binds to the state
+preserve' (Ltt binds bdy _) = preserve' bdy -- TODO: add binds to the state
+preserve' (Ltr binds bdy _) = preserve' bdy -- TODO: add binds to the state
+preserve' (Lrr binds bdy _) = preserve' bdy -- TODO: add binds to the state
 -- | PP-CONCAT
 preserve' (Bgn [] _) = return True
 preserve' (Bgn (e:es) x) = do b <- preserve' e 
                               b' <- preserve' (Bgn es x)
                               return $ b && b'
 -- |PP-IF
-preserve' (Iff b c a _) = do bc <- preserve' c 
-                             ba <- preserve' a 
+preserve' (Iff b c a _) = do (s, g) <- get; bc <- preserve' c 
+                             put (s, g); ba <- preserve' a -- we need to ensure that both branches are checked for the same state
                              return $ bc && ba
--- | PP-APP *
+-- | PP-APP * (could modify the state in the body of the function)
 preserve' (App prc ops _) = return False -- TODO: check if PP holds for the body of the applied function
--- | PP-SKIP
+-- | PP-SKIP (all other expressions don't modify the state and as such are equivalent to skip)
 preserve' _ = return True
 
 
