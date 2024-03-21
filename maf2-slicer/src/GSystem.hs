05a1f7ef4229ebac8cd58b4bcddb30112a255197
Sarah Verbelen <sarah.verbelen2000@gmail.com>
2024-03-21 12:40:09 +0100

    keep agreement in state for labelExp

--- maf2-slicer/src/GSystem.hs
+++ maf2-slicer/src/GSystem.hs
@@ -10,35 +10,44 @@ import Dependency.State
 import Syntax.Scheme.AST
 
 import Control.Monad.State
+import Data.Tuple.Extra(snd3)
 
 type Labels = [(Span, Agreement)]
 
 -- label all statements in the sequence with agreements by backwards propagating the G-system rules
 labelSequence :: forall v . (Eq v) => Exp -> Agreement -> Labels
-labelSequence e g = snd $ execState (labelExp @v e g) ((const True), [])
+labelSequence e g = snd3 $ execState (labelExp @v e) ((const True), [], g)
 
-labelExp :: forall v . (Eq v) => Exp -> Agreement -> State ((AbstractSto v -> Bool), Labels) Agreement
+labelExp :: forall v . (Eq v) => Exp -> State ((AbstractSto v -> Bool), Labels, Agreement) Agreement
 -- | G-PP TODO
 -- | G-CONCAT
-labelExp (Bgn (e':es) x) g = do 
-    g' <- labelExp @v (Bgn es x) g 
-    labelExp @v e' g'
--- (| G-SUB TODO?)
+labelExp (Bgn (e':es) x) = do 
+    labelExp @v (Bgn es x) 
+    labelExp @v e'
 -- | G-ASSIGN TODO
-labelExp (Dfv var e s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
-labelExp (Dff var args bdy s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
-labelExp (Set var e s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
--- | G-IF TODO
--- | G-IF2
-labelExp (Iff e a b s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
--- (| G-IF1: agreement holds no matter which path is taken (TODO: do I need both if rules? or could I get away with only if2 and take the hit to accuracy))
---labelExp' (Iff e a b s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
--- | G-LET * TODO
-labelExp (Let bds bdy s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
-labelExp (Ltt bds bdy s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
-labelExp (Ltr bds bdy s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
-labelExp (Lrr bds bdy s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
+labelExp (Dfv var e s) = labelBinding (var, e, s) 
+labelExp (Set var e s) = labelBinding (var, e, s)
+--labelExp (Dff var args bdy s) g = 
+-- | G-IF
+labelExp (Iff e a b s) = labelIf e a b s
+-- | G-LET
+labelExp (Let bds bdy s) = labelLet bds bdy s
+labelExp (Ltt bds bdy s) = labelLet bds bdy s
+labelExp (Ltr bds bdy s) = labelLet bds bdy s
+labelExp (Lrr bds bdy s) = labelLet bds bdy s
 -- | G-APP * TODO
-labelExp (App prc ops s) g = do modify (\(p, lbls) -> (p, (s, g):lbls)); return g 
+labelExp (App prc ops s) = do modify (\(p, lbls, g) -> (p, (s, g):lbls, g)); (_, _, g) <- get; return g 
 -- | G-SKIP
-labelExp e g = do modify (\(p, lbls) -> (p, (spanOf e, g):lbls)); return g 
+labelExp e = do modify (\(p, lbls, g) -> (p, (spanOf e, g):lbls, g)); (_, _, g) <- get; return g 
+
+-- | G-LET TODO
+labelLet :: forall v. (Eq v) =>  [(Ide, Exp)] -> Exp -> Span -> State ((AbstractSto v -> Bool), Labels, Agreement) Agreement
+labelLet bds bdy s = do modify (\(p, lbls, g) -> (p, (s, g):lbls, g)); (_, _, g) <- get; return g  
+ 
+-- | G-ASSIGN TODO
+labelBinding :: forall v . (Eq v) => (Ide, Exp, Span) -> State ((AbstractSto v -> Bool), Labels, Agreement) Agreement
+labelBinding (var, e, s) = do modify (\(p, lbls, g) -> (p, (s, g):lbls, g)); (_, _, g) <- get; return g 
+
+-- | G-IF TODO
+labelIf :: forall v . (Eq v) => Exp -> Exp -> Exp -> Span -> State ((AbstractSto v -> Bool), Labels, Agreement) Agreement
+labelIf e a b s = do modify (\(p, lbls, g) -> (p, (s, g):lbls, g)); (_, _, g) <- get; return g 
