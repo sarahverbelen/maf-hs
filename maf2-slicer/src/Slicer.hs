2e89f1c4b5c19cbbd2443478f2cb5e9c9bb34b4f
Sarah Verbelen <sarah.verbelen2000@gmail.com>
2024-04-01 20:24:00 +0200

    remove labels rom slicestate

--- maf2-slicer/src/Slicer.hs
+++ maf2-slicer/src/Slicer.hs
@@ -19,50 +19,41 @@ slice :: Exp -> Agreement -> Exp
 -- | takes a program and a criterion (= agreement (= list of relevant variables)) and returns the program where only statements affecting the criterion are remaining
 slice p c = sliceExp p $ labelSequence p c
 
-type SliceState = State (AbstractSto V, Labels) Exp 
+type SliceState = State (AbstractSto V) Exp 
 
 sliceExp :: Exp -> Labels -> Exp 
-sliceExp e lbls = evalState (sliceExp'' e) (mempty, lbls) 
+sliceExp e l = evalState (sliceExp'' e l) mempty 
 
-sliceExp'' :: Exp -> SliceState 
-sliceExp'' e = do (_, lbls) <- get 
-                  if isSkip lbls then return Empty else sliceExp' e
+sliceExp'' :: Exp -> Labels -> SliceState 
+sliceExp'' e l = if isSkip l then return Empty else sliceExp' e l
 
-sliceExp' :: Exp -> SliceState 
-sliceExp' e@(Let bds bdy s) = sliceLet e bds bdy s Let 
-sliceExp' e@(Ltt bds bdy s) = sliceLet e bds bdy s Ltt 
-sliceExp' e@(Ltr bds bdy s) = sliceLet e bds bdy s Ltr 
-sliceExp' e@(Lrr bds bdy s) = sliceLet e bds bdy s Lrr
-sliceExp' e@(Dfv _ _ _)     = sliceAssignment e
-sliceExp' e@(Set _ _ _)     = sliceAssignment e   
-sliceExp' (Bgn es s)        = do    (_, lbl) <- get -- possible improvement: remove whole begin if it preserves its label
-                                    let (Begin l lbls) = lbl
-                                    return (Bgn (map (uncurry (sliceExp)) (zip es lbls)) s)                                
+sliceExp' :: Exp -> Labels -> SliceState 
+sliceExp' e@(Let bds bdy s) l       = sliceLet e bds bdy s Let l
+sliceExp' e@(Ltt bds bdy s) l       = sliceLet e bds bdy s Ltt l 
+sliceExp' e@(Ltr bds bdy s) l       = sliceLet e bds bdy s Ltr l 
+sliceExp' e@(Lrr bds bdy s) l       = sliceLet e bds bdy s Lrr l
+sliceExp' e@(Dfv _ _ _) l           = sliceAssignment e l
+sliceExp' e@(Set _ _ _) l           = sliceAssignment e l  
+sliceExp' (Bgn es s) (Begin l lbls) = return (Bgn (map (uncurry (sliceExp)) (zip es lbls)) s)                                
 
+sliceAssignment :: Exp -> Labels -> SliceState 
+sliceAssignment e (Binding g) = do  sto <- get
+                                    let (b, sto') = preserveWithSto sto g e
+                                    put sto'
+                                    if b then return Empty else return e
 
-sliceAssignment :: Exp -> SliceState 
-sliceAssignment e = do  (sto, lbl) <- get
-                        let (Binding g) = lbl
-                        let (b, sto') = preserveWithSto sto g e
-                        put (sto', lbl)
-                        if b then return Empty else return e
+sliceLet :: Exp -> [(Ide, Exp)] -> Exp -> Span -> ([(Ide, Exp)] -> Exp -> Span -> Exp) -> Labels -> SliceState
+sliceLet e bds bdy s let' (Lett g lbls lbl) = do    sto <- get
+                                                    let bdy' = (sliceExp bdy lbl)
+                                                    let (bds', sto') = runState (sliceBindings bds lbls) sto
+                                                    put sto'
+                                                    if (preserve mempty g e) then return Empty else return $ let' bds' bdy' s 
 
-sliceLet :: Exp -> [(Ide, Exp)] -> Exp -> Span -> ([(Ide, Exp)] -> Exp -> Span -> Exp) -> SliceState
-sliceLet e bds bdy s let' = do  (sto, l) <- get 
-                                let (Lett g lbls lbl) = l
-                                let bdy' = (sliceExp bdy lbl)
-                                let (bds', sto') = runState (sliceBindings bds lbls) sto
-                                put (sto', l)
-                                if (preserve mempty g e) then return Empty else return $ let' bds' bdy' s 
-
-type BindingState = State (AbstractSto V) [(Ide, Exp)]
-
-sliceBindings :: [(Ide, Exp)] -> [Labels] -> BindingState
+sliceBindings :: [(Ide, Exp)] -> [Labels] -> State (AbstractSto V) [(Ide, Exp)]
 sliceBindings bds lbls = do bds' <- mapM sliceBinding (zip bds lbls)
                             return $ concat bds'
 
-
-sliceBinding :: ((Ide, Exp), Labels) -> BindingState 
+sliceBinding :: ((Ide, Exp), Labels) -> State (AbstractSto V) [(Ide, Exp)]
 sliceBinding ((var, exp), (Binding g)) = do    s <- get
                                                let (b, s') = preserveWithSto s g (Set var exp NoSpan) 
                                                put s'
