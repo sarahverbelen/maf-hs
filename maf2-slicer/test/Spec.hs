d21e2d9d7052e29188f10d60c0068732ee47e38d
Sarah Verbelen <sarah.verbelen2000@gmail.com>
2024-04-21 13:39:21 +0200

    setup test file

--- maf2-slicer/test/Spec.hs
+++ maf2-slicer/test/Spec.hs
@@ -1,2 +1,27 @@
+import Syntax.Scheme
+
+import Slicer
+import Dependency.State
+import Property.Agreement
+
+import Test.QuickCheck
+import qualified Data.Map as Map
+
+-- generators
+
+-- properties
+
+prop_preserved_semantics :: Exp -> Bool 
+prop_preserved_semantics e = 
+  let var = "x"
+      criterion = [(var, PAll)] -- TODO 
+      e' = slice e criterion
+      s = mempty
+      (_, s1) = abstractEval' e s
+      (_, s2) = abstractEval' e' s
+      v1 = Map.lookup var s1 
+      v2 = Map.lookup var s2  
+  in v1 == v2
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck prop_preserved_semantics
